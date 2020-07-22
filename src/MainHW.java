3a0cbe727b64e0ce6ea3f793aadf39eafe37d127
Alexey Arefev <aref307@gmail.com>
2020-07-23 01:29:42 +0300

    Да, это было жестко

--- src/MainHW.java
+++ src/MainHW.java
@@ -1,69 +1,77 @@
 
-//1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
 public class MainHW {
+
     public static void main(String[] args) {
-        //2. Создать переменные всех пройденных типов данных и инициализировать их значения.
-
-        byte byteExample = -119;
-        short shortExample = 31307;
-        int intExample = 100;
-        long longExample = 307000L;
-        float floatExample = 30.7f;
-        double doubleExample = -307.307;
-        char charExample = '\u0000';
-        boolean boolExample = true;
-
-        //Вывод решенных задач
-        System.out.println(calculate(3,4,5,6));
-        System.out.println(between10and20(7,17));
-        isPositiveOrNegative(8);
-        System.out.println(isNegative(-5));
-        hello("Кеша");
+        /* Вывод результатов домашнего
+        задания
+         */
+        System.out.println("Задача №1:");
+        invertArray(); //Исходный массив {1, 1, 0, 0, 1, 0, 1, 1, 0, 0}
+        System.out.println("\n\nЗадача №2:");
+        fillArray();
+        System.out.println("\n\nЗадача №3:");
+        changeArray(); //Исходный массив {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}
+        System.out.println("\n\nЗадача №4:");
+        fillDiagonal();
 
     }
 
-
-    /*3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
-    где a, b, c, d – аргументы этого метода, имеющие тип float.*/
-    public static float calculate(float a, float b, float c, float d) {
-        return a * (b + (c / d));
-
-    }
-
-    /*4. Написать метод, принимающий на вход два целых числа и проверяющий,
-     что их сумма лежит в пределах от 10 до 20 (включительно),
-     если да – вернуть true, в противном случае – false.*/
-
-    public static boolean between10and20(int a, int b) {
-        int sum = a + b;
-        return (10<=sum && sum <=20);
-    }
-
-    /*Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
-    положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+    /* 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например:
+    [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
      */
+    public static void invertArray() {
+        int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.print("Меняем 1 на 0, 0 на 1 и: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+            }
 
-    public static void isPositiveOrNegative(int a) {
-        if (a >= 0)
-            System.out.println("Число положительное");
-         else
-            System.out.println("Число отрицательное");
+    }
+
+    /* 2.Задать пустой целочисленный массив размером 8.
+    С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+     */
+    public static void fillArray() {
+        int[] arr = new int[8];
+        for (int i = 0, j = 0; i < arr.length; i++, j +=3) {
+            arr[i] = j;
+            System.out.print(arr[i] + " ");
+
         }
-     /*6. Написать метод, которому в качестве параметра передается целое число.
-     Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
-      */
-    public static boolean isNegative (int a) {
-        if (a >=0)
-            return false;
-         else
-            return true;
     }
-    /* 7. Написать метод, которому в качестве параметра передается строка, обозначающая имя.
-    Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+
+    /*3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+     и числа меньшие 6 умножить на 2;
      */
 
-    public static void hello(String name) {
-        System.out.println("Привет, " + name + "!");
+    public static void changeArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 6) arr[i] *= 2;
+                System.out.print(arr[i] + " ");
+            }
+    }
+    /*4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+    и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+     */
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ((i == j) || (i == arr.length - 1 - j)) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+
+        }
     }
 
 
